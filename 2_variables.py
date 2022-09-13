49bca0518abece9cb8773bd56694738c6d9f232e
rahuladepu <rahuladepu@gmail.com>
2022-09-13 16:21:34 -0500

    Adding a Variables Python project

---
+++ 2_variables.py
@@ -0,0 +1,15 @@
+# Integer Value
+age = 20
+
+# String Value
+sentence = "My name is Rahul Adepu"
+
+# Multiple Variable with individual value Assignment
+rahul, soumya, tushar = 25, 25, 24
+
+# Assign multiple variables with same value
+rahul1 = soumya1 = tushar1 = 25
+
+# Assigning multiple types of variable with different types of datatypes
+nameTemp, ageTemp = "My name is Rahul", 25
+
